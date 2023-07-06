503ebbff940f77ca8f9fa40fbbc8b26763b9bc39
hohokie <nhpunch@gmail.com>
2023-07-06 22:40:13 +0900

    feat: add alias path config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '~/lib', replacement: '/src/lib' },
+      { find: '~/hooks', replacement: '/src/hooks' },
+      { find: '~/pages', replacement: '/src/pages' },
+      { find: '~/', replacement: '/src' },
+    ],
+  },
+});
